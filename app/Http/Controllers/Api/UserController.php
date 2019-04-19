ffd25fdfce178e9f712525344e9d26398d9ba1e7
wanwb001 <409329702@qq.com>
2019-04-19 11:23:26 +0800

    0419commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,32 +1,35 @@
 <?php
-
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-
 class UserController extends Controller
 {
-    //
+    //返回用户列表
     public function index(){
-        return 'wanwb001';
+        //3个用户为一页
+        $users = User::paginate(3);
+        return $this->success($users);
+    }
+    //返回单一用户信息
+    public function show(User $user){
+        return $this->success($user);
     }
 
     //用户注册
     public function store(Request $request){
-        
+
         User::create($request->all());
-        return '用户注册成功。。。';
+        return $this->setStatusCode(201)->success('用户注册成功');
     }
    //用户登录
     public function login(Request $request){
         $res=Auth::guard('web')->attempt(['name'=>$request->name,'password'=>$request->password]);
         if($res){
-            return '用户登录成功...';
+            return $this->setStatusCode(201)->success('用户登录成功...');
         }
-        return '用户登录失败';
+        return $this->failed('用户登录失败',401);
     }
 }
